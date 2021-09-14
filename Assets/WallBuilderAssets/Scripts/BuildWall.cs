a54e767f335054026ec0f10f1a5a3c1acc5c03c0
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-09-14 00:12:52 -0700

    Added id to json objects

--- Assets/WallBuilderAssets/Scripts/BuildWall.cs
+++ Assets/WallBuilderAssets/Scripts/BuildWall.cs
@@ -100,6 +100,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
         {
             if (!TryGetTouchPosition(out Vector2 touchPosition))
                 return;
+            // if initial brick already spawned
             if (spawnedWall != null){
                 Ray ray = origin.camera.ScreenPointToRay(Input.GetTouch(0).position);
                 RaycastHit hitObject;
@@ -109,77 +110,80 @@ namespace UnityEngine.XR.ARFoundation.Samples
                         addBrick(snapObject);
                     }
                 }
-            }
+            }// case for initial brick
             else if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
             {
                 // Raycast hits are sorted by distance, so the first one
                 // will be the closest hit.
                 var hitPose = s_Hits[0].pose;
                 wallRotation = Quaternion.Euler(0, origin.camera.transform.rotation.eulerAngles.y, 0);
+                // spawn wall
                 spawnedWall = Instantiate(m_PlacedPrefab, hitPose.position, wallRotation);
                 wallController = spawnedWall.GetComponent<Wall>();
+                // spawn initial brick
                 spawnedBrick = Instantiate(brick, hitPose.position, wallRotation);
                 spawnedBrick.GetComponent<Brick>().setColor(brick.GetComponent<Brick>().color);
+                // make brick child of wall
                 spawnedBrick.transform.parent = spawnedWall.transform;
                 Brick newBrick = spawnedBrick.GetComponent<Brick>();
                 newBrick.scale = brick.GetComponent<Brick>().scale;
+                // add snaps to new brick
                 newBrick.addSnaps();
+                // add snaps to our list of all snaps
                 List<Snap> newSnaps = newBrick.getSnaps();
                 foreach(Snap s in newSnaps){
                     wallSnaps.Add(s);
                 }
+                // turn off plane detection to make detecting snaps easier
                 planeDetectionController.TogglePlaneDetection();
-                // add brick to wall
+                // add bricks to our list of bricks and enable saving the wall
                 storedBricks.Add(newBrick);
-                
+                newBrick.id = index;
                 
             }
         }
 
         void addBrick(Snap snapObject){
+            // Look for the snap in our list of snaps
             foreach (Snap cur in wallSnaps){
                 if(snapObject == cur){
+                    // get a reference to the parent
                     Brick parentBrick = cur.GetComponentInParent<Brick>();
                     Brick addedBrick;
+                    // depending on the side of the snap, spawn a brick
                     if(cur.isTop){
                         addedBrick = parentBrick.addTopBrick(brick.GetComponent<Brick>().color);
-                        List<Snap> newSnaps = addedBrick.getSnaps();
-                        foreach(Snap s in newSnaps){
-                            wallSnaps.Add(s);
-                        }
-                        // add brick to wall
-                        storedBricks.Add(addedBrick);
-                        wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, "up");
-                        index++;
+                        processBrick(addedBrick, parentBrick, "up");
                     }else if(cur.isRight){
                         addedBrick = parentBrick.addRightBrick(brick.GetComponent<Brick>().color);
-                        List<Snap> newSnaps = addedBrick.getSnaps();
-                        foreach(Snap s in newSnaps){
-                            wallSnaps.Add(s);
-                        }
-                        // add brick to wall
-                        storedBricks.Add(addedBrick);
-                        wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, "right");
-                        index++;
+                        processBrick(addedBrick, parentBrick, "right");
                     }else if(cur.isLeft){
                         addedBrick = parentBrick.addLeftBrick(brick.GetComponent<Brick>().color);
-                        List<Snap> newSnaps = addedBrick.getSnaps();
-                        foreach(Snap s in newSnaps){
-                            wallSnaps.Add(s);
-                        }
-                        // add brick to wall
-                        storedBricks.Add(addedBrick);
-                        wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, "left");
-                        index++;
+                        processBrick(addedBrick, parentBrick, "left");
                     }
                     
                 }
             }
         }
+        private void processBrick(Brick addedBrick, Brick parentBrick, string dir){
+            // add snaps to list of all snaps
+            List<Snap> newSnaps = addedBrick.getSnaps();
+            foreach(Snap s in newSnaps){
+                wallSnaps.Add(s);
+            }
+
+            // add brick to list of bricks
+            storedBricks.Add(addedBrick);
+            // store brick in file handler for json serialization
+            wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, dir, parentBrick.id);
+            // give the brick a new id
+            index++;
+            addedBrick.id = index;
+        }
 
         public void saveWall(){
             if(spawnedWall != null){
-                wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, "null");
+                wallController.AddBrickToList(storedBricks[index].color, storedBricks[index].scale, "null", index);
                 FileHandler.SaveToJSON<BrickHandler>(wallController.placedBricks, "SavedWall");
             }
             
--- Assets/WallBuilderAssets/Scripts/Wall.cs
+++ Assets/WallBuilderAssets/Scripts/Wall.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    
+    // list of stored bricks ready for serialization   
     public List<BrickHandler> placedBricks = new List<BrickHandler>();
 
-    public void AddBrickToList(Color color, int scale, string next){
-        placedBricks.Add(new BrickHandler (color.ToString(), scale, next));
+    public void AddBrickToList(Color color, int scale, string next, int pId){
+        placedBricks.Add(new BrickHandler (color.ToString(), scale, next, pId));
     }
 }
